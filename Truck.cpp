4e1e39197d261916bfc3006f80a1a4a30d3d0e94
Kadeema Wakha <kadeemawakha@gmail.com>
2026-06-19 17:54:47 -0400

    Finished "truck.cpp" and "main.cpp"

--- Truck.cpp
+++ Truck.cpp
@@ -1,2 +1,75 @@
 #include <iostream>
 using namespace std;
+#include <cmath>
+#include "Truck.h"
+
+// truck class implementation
+
+Truck::Truck()
+{
+	truckID = 0;
+	driver = Driver();
+	location = Location();
+}
+
+// constructor to initialize the truck with given values
+
+Truck::Truck(int id, Driver d, Location l)
+{
+	truckID = id;
+	driver = d;
+	location = l;
+}
+
+// setter to set the truck ID
+
+void Truck::setTruckID(int id)
+{
+	truckID = id;
+}
+
+// setter to set the driver
+
+void Truck::setDriver(Driver d)
+{
+	driver = d;
+}
+
+// setter to set the location
+
+void Truck::setLocation(Location l)
+{
+	location = l;
+}
+
+// getter to retrieve the truck ID
+
+int Truck::getTruckID() const
+{
+	return truckID;
+}
+
+// getter to retrieve the driver
+
+Driver Truck::getDriver() const
+{
+	return driver;
+}
+
+// getter to retrieve the location
+
+Location Truck::getLocation() const
+{
+	return location;
+}
+
+// function to calculate the distance from the truck's current location to another location
+
+double Truck::distanceFrom(Location otherLocation) const
+{
+	// calculate the distance using the distance formula: sqrt((x2 - x1)^2 + (y2 - y1)^2)
+
+	double xDiff = location.getX() - otherLocation.getX();
+	double yDiff = location.getY() - otherLocation.getY();
+	return sqrt(xDiff * xDiff + yDiff * yDiff);
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,43 @@
 #include <iostream>
+#include <cmath>
+#include "Driver.h"
+#include "Location.h"
+#include "Truck.h"
 using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	// create sample drivers
+
+	Driver driver1("Liz", "Brian", 35);
+	Driver driver2("Mike", "Smith", 69);
+
+	// create sample locations
+
+	Location location1(2, 3);
+	Location location2(5, 2);
+
+	// create sample trucks
+
+	Truck truck1(178534, driver1, location1);
+	Truck truck2(245817, driver2, location2);
+
+	// origin point
+
+	Location origin(0, 0);
+
+	// calculate distance from origin to each truck
+
+	double distance0 = truck1.distanceFrom(truck2.getLocation());
+
+	double distance1 = truck1.distanceFrom(origin);
+	double distance2 = truck2.distanceFrom(origin);
+
+	// print the results
+
+	cout << "Distance from Truck 1 to Truck 2: " << distance0 << endl;
+	cout << "\nDistance from Truck 1 to Origin: " << distance1 << "\nThe ID of the driver of Truck 1 is: " << truck1.getDriver().getDriverID() << endl;
+	cout << "\nDistance from Truck 2 to Origin: " << distance2 << "\nThe ID of the driver of Truck 2 is: " << truck2.getDriver().getDriverID() << endl;
+
+	return 0;
 }
