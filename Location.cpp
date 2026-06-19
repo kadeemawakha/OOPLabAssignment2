4dc9b7d36b0779985584e673c0befc661c734e27
alievaz06 <seyitalievaz@gmail.com>
2026-06-19 18:39:40 -0400

    Update Location.cpp
    implemented the location class

--- Location.cpp
+++ Location.cpp
@@ -1,2 +1,39 @@
-#include <iostream>
-using namespace std;
+#include "Location.h"
+
+// default constructorr
+Location::Location()
+{
+    x = 0;
+    y = 0;
+}
+
+// constructor with parameters
+Location::Location(double xValue, double yValue)
+{
+    x = xValue;
+    y = yValue;
+}
+
+// setter for x
+void Location::setX(double xValue)
+{
+    x = xValue;
+}
+
+// setter for the y
+void Location::setY(double yValue)
+{
+    y = yValue;
+}
+
+// getter for the x
+double Location::getX() const
+{
+    return x;
+}
+
+// getter for y
+double Location::getY() const
+{
+    return y;
+}
