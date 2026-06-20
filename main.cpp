88e900a39ef5681704b17d54c7becf26ed476cf7
Kadeema Wakha <kadeemawakha@gmail.com>
2026-06-20 18:41:29 -0400

    Fixed main.cpp program and added names

--- main.cpp
+++ main.cpp
@@ -2,12 +2,15 @@
 #include <cmath>
 #include "Driver.h"
 #include "Location.h"
-#include "Truck.h"
+#include "Truck.h" 
 using namespace std;
+
+// Group 8 Object Oriented Programming Assignment 2
+// Kadeema, Naomi, Zulfira
 
 int main()
 {
-	// create sample drivers
+	// use the given information to create drivers, locations, and trucks
 
 	Driver driver1("Liz", "Brian", 35);
 	Driver driver2("Mike", "Smith", 69);
@@ -36,8 +39,9 @@ int main()
 	// print the results
 
 	cout << "Distance from Truck 1 to Truck 2: " << distance0 << endl;
-	cout << "\nDistance from Truck 1 to Origin: " << distance1 << "\nThe ID of the driver of Truck 1 is: " << truck1.getDriver().getDriverID() << endl;
-	cout << "\nDistance from Truck 2 to Origin: " << distance2 << "\nThe ID of the driver of Truck 2 is: " << truck2.getDriver().getDriverID() << endl;
+	cout << "\nDistance from Truck 1 to Origin: " << distance1 << "\nThe ID of Truck 1 is: " << truck1.getTruckID() << "\nThe ID of the driver of Truck 1 is: " << truck1.getDriver().getDriverID() << endl;
+	cout << "\nDistance from Truck 2 to Origin: " << distance2 << "\nThe ID of Truck 2 is: " << truck2.getTruckID() << "\nThe ID of the driver of Truck 2 is: " << truck2.getDriver().getDriverID() << endl;
 
 	return 0;
+
 }
